a4ab599843852e4134aef6ddfa544c891dfd39d7
erxn-mcg <87203096+erxn-mcg@users.noreply.github.com>
2021-08-29 16:59:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+//connect to mongodb
+const CONNECTION_URL = 'mongodb+srv://erinthebest:erinthebest123@cluster0.eyhl2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 500;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
---
+++ src/components/Swap/Swap.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+const Swap = () => {
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='erxn'
+			userSecret='mcg'
+			projectID='ea83f66c-da73-4124-8ab8-f564634b89e7'
+		/>
+	);
+}
+
+export default Swap;
